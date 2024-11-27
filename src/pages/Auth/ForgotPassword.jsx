32db90670a239d1038b226e0f86e9938f4b024f2
RaiyanJiyon <raiyanjiyon814@gmail.com>
2024-11-27 09:47:13 +0600

    feat: add Firebase initialization - Created utils/firebase.js - Configured Firebase with environment variables - Initialized Firebase app for authentication

--- src/pages/Auth/ForgotPassword.jsx
+++ src/pages/Auth/ForgotPassword.jsx
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom";
+import Button from "../../components/ui/Button";
 
 const ForgotPassword = () => {
     return (
@@ -17,7 +18,7 @@ const ForgotPassword = () => {
                                 <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900">Your email</label>
                                 <input type="email" name="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" placeholder="name@company.com" required />
                             </div>
-                            <button type="submit" className="w-full text-white bg-[#E1713B] hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-bold rounded-lg text-sm px-5 py-2.5 text-center">Reset Password</button>
+                            <Button text="Reset Password" />
                         </form>
                     </div>
                 </div>
