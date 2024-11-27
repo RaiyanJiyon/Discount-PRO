6059ef8488afd34b8f64a3b55e710e3ad5e92dab
RaiyanJiyon <raiyanjiyon814@gmail.com>
2024-11-27 09:03:25 +0600

    feat: Add user registration page
    - Created Register.jsx for user registration
    - Implemented form fields for name, photo URL, email, password, and confirm password
    - Added social login buttons for Google and GitHub
    - Included terms and conditions checkbox
    - Styled the component with Tailwind CSS
    - Linked to login page for existing users
    - Added Button component in components/ui

--- src/components/ui/Button.jsx
+++ src/components/ui/Button.jsx
@@ -1,8 +1,6 @@
-const Button = () => {
+const Button = ({ text }) => {
     return (
-        <div>
-            
-        </div>
+        <button type="submit" className="w-full bg-[#E1713B] text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-bold rounded-lg text-sm px-5 py-2.5 text-center">{text}</button>
     );
 };
 
--- src/pages/Auth/Register.jsx
+++ src/pages/Auth/Register.jsx
@@ -1,8 +1,69 @@
+import logo from '../../assets/images/logo.png'
+import { FaGithub, FaGoogle } from "react-icons/fa";
+import { Link } from "react-router-dom";
+import Button from '../../components/ui/Button';
+
 const Register = () => {
     return (
-        <div>
-            
-        </div>
+        <section className="bg-gray-50">
+            <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto mt-6 lg:py-0">
+                <Link to={"/"} className="flex items-center mb-6 text-2xl font-semibold text-gray-900">
+                    <img className="w-8 h-8 mr-2" src={logo} alt="logo" />
+                    Discount Pro
+                </Link>
+                <div className="w-full bg-white rounded-lg shadow border md:mt-0 sm:max-w-md xl:p-0">
+                    <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+                        <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl">
+                            Create an account
+                        </h1>
+                        <form className="space-y-4 md:space-y-6" action="#">
+                            <div className="flex flex-col md:flex-row items-center justify-between gap-2">
+                                <div className="flex items-center md:justify-center gap-2 w-full border border-gray-300 px-4 py-2 rounded-lg cursor-pointer">
+                                    <FaGoogle className="text-xl" />
+                                    <span className="text-xs font-medium">Sign up with Google</span>
+                                </div>
+                                <div className="flex items-center md:justify-center gap-2 w-full border border-gray-300 px-4 py-2 rounded-lg cursor-pointer">
+                                    <FaGithub className="text-xl" />
+                                    <span className="text-xs font-medium">Sign up with Github</span>
+                                </div>
+                            </div>
+                            <div>
+                                <label htmlFor="name" className="block mb-2 text-sm font-medium text-gray-900">Your name</label>
+                                <input type="text" name="name" id="name" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" placeholder="name" required />
+                            </div>
+                            <div>
+                                <label htmlFor="photoURL" className="block mb-2 text-sm font-medium text-gray-900">Your photo url</label>
+                                <input type="text" name="photoURL" id="photoURL" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" placeholder="photo url" required />
+                            </div>
+                            <div>
+                                <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900">Your email</label>
+                                <input type="email" name="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" placeholder="name@company.com" required />
+                            </div>
+                            <div>
+                                <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900">Password</label>
+                                <input type="password" name="password" id="password" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" required />
+                            </div>
+                            <div>
+                                <label htmlFor="confirm-password" className="block mb-2 text-sm font-medium text-gray-900">Confirm password</label>
+                                <input type="password" name="confirm-password" id="confirm-password" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" required />
+                            </div>
+                            <div className="flex items-start">
+                                <div className="flex items-center h-5">
+                                    <input id="terms" aria-describedby="terms" type="checkbox" className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300" required />
+                                </div>
+                                <div className="ml-3 text-sm">
+                                    <label htmlFor="terms" className="font-light text-gray-500">I accept the <a className="font-medium text-primary-600 hover:underline" href="#">Terms and Conditions</a></label>
+                                </div>
+                            </div>
+                            <Button text={"Create an account"} />
+                            <p className="text-sm font-light text-gray-500">
+                                Already have an account? <Link to={"/auth/login"} className="font-medium text-[#E1713B] hover:underline">Login here</Link>
+                            </p>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </section>
     );
 };
 
