b26a56121c0e14f9a8b09917211f4b5cd72e94ab
RaiyanJiyon <raiyanjiyon814@gmail.com>
2024-11-28 14:31:58 +0600

    feat: Add CouponCard component and integrate into BrandDetails - Created CouponCard.jsx inside components/brands - Implemented coupon display with details such as coupon code, description, expiry date, condition, and type - Enhanced card styling with Tailwind CSS for better visual appeal - Added action buttons for copying coupon code and using the coupon - Integrated CouponCard component into BrandDetails.jsx for displaying coupons of the selected brand

--- src/pages/Brands/BrandDetails.jsx
+++ src/pages/Brands/BrandDetails.jsx
@@ -1,6 +1,8 @@
 import { useLoaderData, useParams } from "react-router-dom";
 import Navbar from "../../components/shared/Navbar";
 import BrandProfile from "../../components/brands/BrandProfile";
+import CouponCard from "../../components/brands/CouponCard";
+import Footer from "../../components/shared/Footer";
 
 const BrandDetails = () => {
     const loaderBrands = useLoaderData();
@@ -10,12 +12,17 @@ const BrandDetails = () => {
 
     return (
         <div>
-            <Navbar />
+            <div>
+                <Navbar />
+            </div>
 
             {filterBrands.length > 0 ? (
                 <div className="flex justify-center items-center">
                     {filterBrands.map(brand => (
+                        <div key={brand.id}>
                         <BrandProfile key={brand.id} brand={brand} />
+                        <CouponCard key={brand.id} brand={brand} />
+                        </div>
                     ))}
                 </div>
             ) : (
@@ -23,6 +30,10 @@ const BrandDetails = () => {
                     <p className="text-red-500 text-lg font-bold">Brand not found</p>
                 </div>
             )}
+
+            <div>
+                <Footer />
+            </div>
         </div>
     );
 };
